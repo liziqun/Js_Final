ddca504762eff6e5034ef17c23ded99b5808749e
Ziqun Li <52318154+liziqun@users.noreply.github.com>
2020-05-09 00:39:20 -0400

    final submit

---
+++ js/settings.js
@@ -0,0 +1,30 @@
+// color
+var theOpac   = 1;
+
+// basemap settings
+var theCenter = [121.49917602539062,30.957531659727318]; // center point
+var theZoom   = 9.5;   //zoom
+var theBear   = -25;  // rotation angle  N:0，E:90，W:270
+var thePitch  = 60;   // depression angle，top:0
+
+// mapStyle setting
+var mapStyle  = 'mapbox://styles/yipeiz/cjb788d051eue2rp5hhq2jw2h';
+var mapAccessToken = 'pk.eyJ1IjoieWlwZWl6IiwiYSI6ImNpeWF3MDdvZDAwZDkzMXA5NXlsN2FwM3IifQ.y9QTjGM10eunho1JKQnt-g';
+
+// Bar Settings
+var barSize   = 0.5;
+var barCorner = 0.1;
+
+// Light Settings
+var theShadow = true; // whether to set the shadow
+var theAlpha  = 0; // hight
+var theBeta   = 5; // direction
+
+// axis settings
+var timestamp = 3000;
+var auto= true;
+var or = 'vertical';  // 'horizontal' or 'vertical'
+var t = '10%';   // top
+var b = '43%';	 // bottom
+var l = '4%';	 // left
+var w = '7%';	 // width
